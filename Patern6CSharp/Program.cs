7d5a2a22ba3c67440dc68e104dce791edc08466c
Pain31415 <Cherniy412@ukr.net>
2024-03-17 12:14:16 +0200

    Add project files.

---
+++ Patern6CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Patern6CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
